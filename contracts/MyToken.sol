bd88befa3374d82a2919f5238d4c833b2d873cbf
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2021-12-22 14:59:43 -0500

    added ERC token contract

---
+++ contracts/MyToken.sol
@@ -0,0 +1,12 @@
+
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("StarDucks Capu-Token", "SCT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
