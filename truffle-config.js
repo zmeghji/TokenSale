5dcdc71a0c34769fdc2591e92c02f018c107a46d
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2021-12-22 15:53:13 -0500

    added unit tests for token contract

--- truffle-config.js
+++ truffle-config.js
@@ -5,8 +5,11 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
+    //use Ganache
     develop: {
-      port: 8545
+      port: 7545,
+      host: "127.0.0.1",
+      network_id: 5777
     }
   },
   compilers: {
