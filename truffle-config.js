f60e42985ba458c1dda2359f36cde680e78d5d5a
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2021-12-23 15:07:46 -0500

    used hdwallet for deploying contracts

--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,23 @@
 const path = require("path");
+require('dotenv').config({path: './.env'});
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const MetaMaskAccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    //use Ganache
     develop: {
       port: 8545,
       host: "127.0.0.1",
       network_id: 1337
 
+    },
+    ganache_local: {
+      provider: () =>
+        new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:8545/", MetaMaskAccountIndex ),
+      network_id: 1337
     }
   },
   compilers: {
